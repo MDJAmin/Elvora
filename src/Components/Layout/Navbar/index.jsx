041587619c5693afac298be6bb539e57094330cf
MDJAmin <mohammadamindf@gmail.com>
2025-01-17 19:25:45 +0330

    Clean code

--- src/Components/Layout/Navbar/index.jsx
+++ src/Components/Layout/Navbar/index.jsx
@@ -23,27 +23,37 @@ export default function NavBar() {
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener(
+      "mousedown",
+      handleClickOutside
+    );
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener(
+        "mousedown",
+        handleClickOutside
+      );
     };
   }, []);
 
   return (
-    <nav className="bg-gray-800 py-4 fixed top-0 left-0 w-full z-50">
-      <div className="container mx-auto flex justify-between items-center px-4 md:px-0">
+    <nav className='bg-gray-800 py-4 fixed top-0 left-0 w-full z-50'>
+      <div className='container mx-auto flex justify-between items-center px-4 md:px-0'>
         {/* Logo */}
-        <div className="logo">
-          <img className="w-22 h-12" src={elvoraLogo2} alt="Logo" />
+        <div className='logo'>
+          <img
+            className='w-22 h-12'
+            src={elvoraLogo2}
+            alt='Logo'
+          />
         </div>
 
         {/* Hamburger Menu */}
         <button
           ref={buttonRef}
-          className="text-white md:hidden focus:outline-none"
+          className='text-white md:hidden focus:outline-none'
           onClick={toggleMenu}
         >
-          <FaBars className="h-6 w-6" />
+          <FaBars className='h-6 w-6' />
         </button>
 
         {/* Menu Items */}
@@ -55,50 +65,50 @@ export default function NavBar() {
         >
           <li>
             <a
-              href="#"
-              className="text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300"
+              href='#'
+              className='text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300'
             >
               Home
             </a>
           </li>
-          <li className="relative group">
+          <li className='relative group'>
             <a
-              href="#"
-              className="text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300"
+              href='#'
+              className='text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300'
             >
               Our Services
             </a>
 
             {/* Dropdown Menu */}
-            <ul className="absolute left-0 top-full bg-gray-800 py-2 w-48 hidden group-hover:flex flex-col shadow-lg z-10">
-              <li className="py-2 px-4 hover:bg-gray-700 cursor-pointer">
+            <ul className='absolute left-0 top-full bg-gray-800 py-2 w-48 hidden group-hover:flex flex-col shadow-lg z-10'>
+              <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer'>
                 <a
-                  href="#"
-                  className="text-white transition duration-300"
+                  href='#'
+                  className='text-white transition duration-300'
                 >
                   Catering and Lobby Services
                 </a>
               </li>
-              <li className="py-2 px-4 hover:bg-gray-700 cursor-pointer">
+              <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer'>
                 <a
-                  href="#"
-                  className="text-white transition duration-300"
+                  href='#'
+                  className='text-white transition duration-300'
                 >
                   Services of Rooms and Units
                 </a>
               </li>
-              <li className="py-2 px-4 hover:bg-gray-700 cursor-pointer">
+              <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer'>
                 <a
-                  href="#"
-                  className="text-white transition duration-300"
+                  href='#'
+                  className='text-white transition duration-300'
                 >
                   Transportation Services
                 </a>
               </li>
-              <li className="py-2 px-4 hover:bg-gray-700 cursor-pointer">
+              <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer'>
                 <a
-                  href="#"
-                  className="text-white transition duration-300"
+                  href='#'
+                  className='text-white transition duration-300'
                 >
                   Management Services and Support for Daily
                   Needs
@@ -108,16 +118,16 @@ export default function NavBar() {
           </li>
           <li>
             <a
-              href="#"
-              className="text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300"
+              href='#'
+              className='text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300'
             >
               About Us
             </a>
           </li>
           <li>
             <a
-              href="#"
-              className="text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300"
+              href='#'
+              className='text-white block px-4 py-2 md:py-0 hover:text-[#878127] transition duration-300'
             >
               Reservation & Contact Us
             </a>
