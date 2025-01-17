b63abaf6796b6f0804d5a130d8dc755f1fa57879
MDJAmin <mohammadamindf@gmail.com>
2025-01-17 18:53:01 +0330

    Feat : organize components

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -2,4 +2,13 @@
 import elvoraLogo from "../Assets/Logo/Elvora-Logo.png"
 import elvoraLogo2 from "../Assets/Logo/Elvora-Logo-2.png"
 
-export { elvoraLogo , elvoraLogo2 }
+export { elvoraLogo, elvoraLogo2 }
+
+
+// Home Page section's
+import Hero from "../Components/Layout/Hero"
+import NavBar from "../Components/Layout/Navbar";
+import ServicesCards from "../Components/Common/ServicesCards";
+import Footer from "../Components/Layout/Footer";
+
+export {Hero, NavBar, ServicesCards, Footer}
