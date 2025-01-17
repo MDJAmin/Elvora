4b6e358a6eec9771962d3a7f6cd405c2c35e277d
MDJAmin <mohammadamindf@gmail.com>
2025-01-17 17:20:33 +0330

    Feat : home page

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import NavBar from "../../Components/Layout/Navbar";
+import Footer from "../../Components/Layout/Footer";
+import HeroImg from "../../Assets/Images/HeroSectionImg.jpg";
+import ServicesCards from "../../Components/Common/ServicesCards";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+
+      <section className='relative h-screen z-40'>
+        <img
+          src={HeroImg}
+          alt='Hero Section'
+          className='absolute inset-0 w-full h-full object-cover'
+        />
+        <div className='absolute inset-0 bg-black bg-opacity-50'></div>
+
+        <div className='relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4'>
+          <h2 className='text-4xl md:text-6xl font-bold mb-6'>
+            Elvora: A Reliable Experienced
+          </h2>
+          <p className='text-lg md:text-xl mb-8'>
+            Delivering excellence and innovation in every
+            solution.
+          </p>
+          <a
+            href='#about-us'
+            className='bg-gray-800 hover:bg-gray-700 text-white py-3 px-6 rounded-lg text-lg transition duration-300'
+          >
+            Learn More
+          </a>
+        </div>
+      </section>
+      <ServicesCards />
+      <Footer />
+    </>
+  );
+}
