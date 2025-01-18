c383a7378ed5965c71f22e4485bd871ac0a45f89
MDJAmin <mohammadamindf@gmail.com>
2025-01-18 09:42:45 +0330

    Feat: Context (global state)

---
+++ src/Constants/translations.js
@@ -0,0 +1,21 @@
+export const translations = {
+    English: {
+      home: "Home",
+      services: "Our Services",
+      about: "About Us",
+      contact: "Reservation & Contact Us",
+    },
+    Persian: {
+      home: "خانه",
+      services: "خدمات ما",
+      about: "درباره ما",
+      contact: "رزرو و تماس با ما",
+    },
+    Arabic: {
+      home: "الرئيسية",
+      services: "خدماتنا",
+      about: "معلومات عنا",
+      contact: "الحجز والتواصل",
+    },
+  };
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { LanguageProvider } from "./Context/LanguageContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <LanguageProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </LanguageProvider>
+);
